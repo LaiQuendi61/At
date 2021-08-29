cc27f7ec3e4d9596cfd6fd26556a60fb47a890a9
Alperen Ozlu <alperenozlu@gmail.com>
2021-08-29 21:54:28 +0300

    Larger image on click

--- html 2/jquery.js
+++ html 2/jquery.js
@@ -1,20 +1,7 @@
-$(document).ready(function(){
-    $("#question").click(function(){
-        if($("#question").css('height') == "50%"){
-           $("#question").animate({height: '100%',width: '100%'});
-        } 
-        else{
-           $("#question").animate({height: '50%',width: '50%'});
-        };
-
-        /*$("#question").animate(
-            {
-                height: "100%",
-                width: "100%"
-            }
-        );*/
-
-
-        //$(".jhide").toggle();
-      });
-   });
+$(document).ready(function () {
+    const questionSelector = $("#question");
+    questionSelector.click(function () {
+        questionSelector.toggleClass('full')
+        $(".jhide").toggle();
+    });
+});
