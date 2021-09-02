a0fa1f424ad75c79beb9dc2274f24e38ad25e727
Alp <31357901+LaiQuendi61@users.noreply.github.com>
2021-09-02 21:31:47 +0300

    Add files via upload
    gece modu

--- html 2/jquery.js
+++ html 2/jquery.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     const questionSelector = $("#question");
+    const at = 
+
     questionSelector.click(function () {
         questionSelector.toggleClass('full')
         $(".jhide").toggle();
     });
     $("#daire").click(function(){
-        $(":root").css("background-color","#222222");
-        $(".btn").css("background-color","rgb(58, 58, 58)");
-        $(".btn").css("border","black");
+        $("body").toggleClass('gece-modu');
     });
 });
